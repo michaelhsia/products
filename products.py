1cca89a4352719f396a40a6a7d00e822a182f855
Michael Hsia <michaelhsia8716@gmail.com>
2021-10-03 12:40:07 +0800

    add header

--- products.py
+++ products.py
@@ -18,3 +18,15 @@ for p in products:
 #p = [name, price] 等於 10-12 行
 
 #products = append([name, price]) 等於 10-13 行
+
+with open ('products.csv', 'w', encoding='utf-8') as f: #加入utf-8(最廣泛使用的編碼), 才能正常讀取/寫入) 
+                                                        #寫入檔案, 如果資料夾中沒有會建立檔案, 如果有同樣檔案會覆蓋
+    f.write('商品, 價格\n') #','用來分隔csv檔中的每個標題
+    for p in products:
+        f.write(p[0] + ',' + p[1] + '\n') #字串可用 '+', '*'來合併
+                                          #'a' + 'b' = 'ab', 'abc' * 3 = 'abcabcabc'
+
+
+
+
+
